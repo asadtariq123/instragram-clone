6313d3713fe09a68e8b07f56c4ee8a81e4fda256
asadtariq123 <asadtariq970@gmail.com>
2021-11-30 21:57:52 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Let make Instahram Clone</h1>
+      <h1>Let make Instagram Clone with React js booom</h1>
     </div>
   );
 }
